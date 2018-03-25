815b8e65530fea8c1ccb8e5f6ae8d9be1f424e37
fl1ghtl0ger <fl1ghtl0ger@gmail.com>
2018-03-25 17:00:20 +0300

    architecture and data augmentation fixed

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ for image, measurement in zip(images, measurements):
     augmented_images.append(image)
     augmented_measurements.append(measurement)
     flipped_image = cv2.flip(image, 1)
-    flipped_measurement = float(measurement) * 1.0
+    flipped_measurement = float(measurement) * -1.0
     augmented_images.append(flipped_image)
     augmented_measurements.append(flipped_measurement)
     
@@ -49,14 +49,19 @@ from keras.layers.pooling import MaxPooling2D
 #lenet implemented
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))                                  
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(16, 5, 5, activation='relu'))
+model.add(Convolution2D(12, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(18, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
